9289d69405eb11ed77c1b1ad6004e9ecf264da3d
Caiottk <caionarutolegal@gmail.com>
2024-03-10 11:34:07 -0300

    first commit

---
+++ src/test/java/com/myspace/SpringProject/WebServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspace.SpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
